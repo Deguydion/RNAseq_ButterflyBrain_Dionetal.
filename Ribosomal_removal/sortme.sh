320518dcae42c8092c932f6ba9bc00486c30bc20
Deguydion <71120292+Deguydion@users.noreply.github.com>
2024-11-21 10:31:26 +0700

    Update sortme.sh

--- Ribosomal_removal/sortme.sh
+++ Ribosomal_removal/sortme.sh
@@ -7,19 +7,12 @@ OUTPUT_DIR="/AMLab_Ext1/Emilie/AB_transcr_2020/sortmerna/rRNA"
 
 # Loop through each pair of files in the input directory
 for INPUT_FILE_R1 in $INPUT_DIR/*_R1.fastq; do
-    # Get the corresponding R2 file
     INPUT_FILE_R2="${INPUT_FILE_R1/_R1.fastq/_R2.fastq}"
-
-    # Get the base name of the file (without the directory and _R1/_R2.fastq extension)
     BASE_NAME=$(basename "$INPUT_FILE_R1" _R1.fastq)
-
-    # Define output files
     OUTPUT_RRNA_R1="$OUTPUT_DIR/${BASE_NAME}_rRNA_R1.fastq"
     OUTPUT_RRNA_R2="$OUTPUT_DIR/${BASE_NAME}_rRNA_R2t.fastq"
     OUTPUT_NON_RRNA_R1="$OUTPUT_DIR/${BASE_NAME}_sortme_R1.fastq"
     OUTPUT_NON_RRNA_R2="$OUTPUT_DIR/${BASE_NAME}_sortme_R2.fastq"
-
-    # Run SortMeRNA
     sortmerna --ref "$DATABASE_DIR/silva-bac-16s-id90.fasta" \
               --reads "$INPUT_FILE_R1" \
               --reads "$INPUT_FILE_R2" \
@@ -28,8 +21,4 @@ for INPUT_FILE_R1 in $INPUT_DIR/*_R1.fastq; do
               --paired_in \
               --fastx \
               --log -v
-
-    echo "Processed $INPUT_FILE_R1 and $INPUT_FILE_R2"
 done
-
-echo "All samples processed."
